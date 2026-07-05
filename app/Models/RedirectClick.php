89b259b32cab09157ed0e766d06d2e011199d4c4
Dev <denis.03.ermakov@gmail.com>
2026-07-05 14:35:10 +0500

    feat: add redirect click persistence

---
+++ app/Models/RedirectClick.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+#[Fillable([
+    'short_link_id',
+    'visitor_ip',
+    'user_agent',
+])]
+class RedirectClick extends Model
+{
+    /**
+     * @return BelongsTo<ShortLink, $this>
+     */
+    public function shortLink(): BelongsTo
+    {
+        return $this->belongsTo(ShortLink::class);
+    }
+}
